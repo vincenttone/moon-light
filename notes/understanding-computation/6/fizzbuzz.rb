bdae31d6ef712ad99a10f5f549e1f89202b58b5a
vincent <tongwensong@gmail.com>
2017-02-07 14:01:00 +0800

    adjust fizzbuzz

--- notes/understanding-computation/6/fizzbuzz.rb
+++ notes/understanding-computation/6/fizzbuzz.rb
@@ -11,22 +11,12 @@ FIVE = -> p { -> x { p[p[p[p[p[x]]]]] } }
 FIFTEEN = -> p { -> x { p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[x]]]]]]]]]]]]]]] } }
 HUNDRED = -> p { -> x { p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[x]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] } }
 
-p to_integer(ZERO)
-p to_integer(ONE)
-p to_integer(TWO)
-p to_integer(THREE)
-p to_integer(FIVE)
-p to_integer(FIFTEEN)
-p to_integer(HUNDRED)
-
 TRUE = -> x { -> y { x }}
 FALSE = -> x { -> y { y }}
 
 def to_boolean(proc)
   proc[true][false]
 end
-
-p to_boolean(TRUE)
 
 IF = -> b {
   -> x {
@@ -36,30 +26,17 @@ IF = -> b {
   }
 }
 
-p IF[TRUE]['happy']['sad']
-p IF[FALSE]['happy']['sad']
-
 def to_boolean(proc)
   IF[proc][true][false]
 end
 
 IF = -> b { b }
 
-p IF[TRUE]['happy']['sad']
-p IF[FALSE]['happy']['sad']
-
 IS_ZERO = -> n { n[-> x { FALSE }][TRUE] }
-
-p to_boolean(IS_ZERO[ZERO])
-p to_boolean(IS_ZERO[THREE])
 
 PAIR = -> x { -> y { -> f { f[x][y]}}}
 LEFT = -> p { p[-> x { -> y { x } } ] }
 RIGHT = -> p { p[-> x { -> y { y } } ] }
-
-my_pair = PAIR[THREE][FIVE]
-p to_integer(LEFT[my_pair])
-p to_integer(RIGHT[my_pair])
 
 INCREMENT = -> n {
   -> p {
@@ -74,10 +51,6 @@ SLIDE = -> p {
 DECREMENT = -> n {
   LEFT[n[SLIDE][PAIR[ZERO][ZERO]]]
 }
-p to_integer(DECREMENT[FIVE])
-p to_integer(DECREMENT[FIFTEEN])
-p to_integer(DECREMENT[HUNDRED])
-p to_integer(DECREMENT[ZERO])
 
 ADD = -> m { -> n { n [INCREMENT] [m] } }
 SUBTRACT = -> m { -> n { n [DECREMENT] [m] } }
@@ -85,19 +58,10 @@ MULTIPLY = -> m { -> n { n [ADD[m]][ZERO] } }
 #MULTIPLY = -> m { -> n { n [ADD [m]][ZERO] } }
 POWER = -> m { -> n { n [MULTIPLY[m] ] [ONE] } }
 
-p to_integer(ADD[FIVE][HUNDRED])
-p to_integer(SUBTRACT[HUNDRED][FIVE])
-p to_integer(MULTIPLY[HUNDRED][FIVE])
-p to_integer(POWER[FIVE][TWO])
-
 IS_LESS_OR_EQUAL = -> m { -> n {
     IS_ZERO[SUBTRACT[m][n]]
   }
 }
-
-p to_boolean(IS_LESS_OR_EQUAL[ONE][TWO])
-p to_boolean(IS_LESS_OR_EQUAL[TWO][TWO])
-p to_boolean(IS_LESS_OR_EQUAL[THREE][TWO])
 
 =begin
 MOD = -> m { -> n {
@@ -124,22 +88,12 @@ MOD =
 m ]
 } } }]
 
-p to_integer(MOD[THREE][TWO])
-p to_integer(MOD[POWER[THREE][THREE]][ADD[THREE][TWO]])
-
 EMPTY = PAIR[TRUE][TRUE]
 UNSHIFT = -> l { -> x {
     PAIR[FALSE][PAIR[x][l]] }}
 IS_EMPTY = LEFT
 FIRST = -> l { LEFT[RIGHT[l]]}
 REST = -> l { RIGHT[RIGHT[l]]}
-
-my_list = UNSHIFT[UNSHIFT[UNSHIFT[EMPTY][THREE]][TWO] ][ONE]
-p to_integer(FIRST[my_list])
-p to_integer(FIRST[REST[my_list]])
-p to_integer(FIRST[REST[REST[my_list]]])
-p to_boolean(IS_EMPTY[my_list])
-p to_boolean(IS_EMPTY[EMPTY])
 
 def to_array(proc)
   array = []
@@ -149,9 +103,6 @@ def to_array(proc)
   end
   array
 end
-
-p to_array(my_list)
-p to_array(my_list).map { |p| to_integer(p) }
 
 RANGE =
        Z[-> f {
@@ -165,9 +116,6 @@ RANGE =
     }  }
 }]
 
-my_range = RANGE[ONE][FIVE]
-p to_array(my_range).map { |p| to_integer(p) }
-
 FOLD = Z[-> f {
          -> l { -> x { -> g {
             IF[IS_EMPTY[l]][
@@ -180,16 +128,12 @@ FOLD = Z[-> f {
          }}}
          }
        ]
-p to_integer(FOLD[RANGE[ONE][FIVE]][ZERO][ADD])
-p to_integer(FOLD[RANGE[ONE][FIVE]][ONE][MULTIPLY])
 MAP =
   -> k { -> f {
     FOLD[k][EMPTY][
       -> l { -> x { UNSHIFT[l][f[x]] } }
     ] }
 }
-my_list = MAP[RANGE[ONE][FIVE]][INCREMENT]
-p to_array(my_list).map { |p| to_integer(p) }
 
 TEN = MULTIPLY[TWO][FIVE]
 B   = TEN
@@ -209,9 +153,6 @@ end
 def to_string(s)
   to_array(s).map { |c| to_char(c) }.join
 end
-
-p to_char(ZED)
-p to_string(FIZZBUZZ)
 
 DIV =
        Z[-> f { -> m { -> n {
@@ -237,27 +178,3 @@ TO_DIGITS =
       }
     ]
   ][MOD[n][TEN]] } }]
-
-p to_array(TO_DIGITS[FIVE]).map { |p| to_integer(p) }
-p to_array(TO_DIGITS[POWER[FIVE][THREE]]).map { |p| to_integer(p) }
-
-solution = 
-  MAP[RANGE[ONE][HUNDRED]][-> n {
-    IF[IS_ZERO[MOD[n][FIFTEEN]]][
-      FIZZBUZZ
-    ][
-      IF[IS_ZERO[MOD[n][THREE]]][
-        FIZZ
-      ][
-        IF[IS_ZERO[MOD[n][FIVE]]][
-          BUZZ
-        ][
-          TO_DIGITS[n]
-        ]
-       ]
-     ]
-}]
-
-to_array(solution).each do |p|
-  puts to_string(p)
-end
