b82f2fd4f2e044a81683a11524860a50ad352cb4
vincent <tongwensong@gmail.com>
2017-02-07 12:23:28 +0800

    fix dtm test rules

--- notes/understanding-computation/5/dtm_test.rb
+++ notes/understanding-computation/5/dtm_test.rb
@@ -52,16 +52,23 @@ p dtm.stuck?
 
 p '-----------'
 p rulebook = DTMRulebook.new([
-                            TMRule.new(1, 'X', 1, 'X', :right),
+                              TMRule.new(1, 'X', 1, 'X', :right),
                               TMRule.new(1, 'a', 2, 'X', :right),
-                            TMRule.new(1, '_', 6, '_', :left),
-                            TMRule.new(4, 'c', 4, 'c', :right),
-                            TMRule.new(4, '_', 5, '_', :left),
-                            TMRule.new(5, 'a', 5, 'a', :left),
-                            TMRule.new(5, 'b', 5, 'b', :left),
-                            TMRule.new(5, 'c', 5, 'c', :left),
-                            TMRule.new(5, 'X', 5, 'X', :left),
-                            TMRule.new(5, '_', 1, '_', :right)
+                              TMRule.new(1, '_', 6, '_', :left),
+                              TMRule.new(2, 'a', 2, 'a', :right),
+                              TMRule.new(2, 'X', 2, 'X', :right),
+                              TMRule.new(2, 'b', 3, 'X', :right),
+                              TMRule.new(3, 'b', 3, 'b', :right),
+                              TMRule.new(3, 'X', 3, 'X', :right),
+                              TMRule.new(3, 'c', 4, 'X', :right),
+
+                              TMRule.new(4, 'c', 4, 'c', :right),
+                              TMRule.new(4, '_', 5, '_', :left),
+                              TMRule.new(5, 'a', 5, 'a', :left),
+                              TMRule.new(5, 'b', 5, 'b', :left),
+                              TMRule.new(5, 'c', 5, 'c', :left),
+                              TMRule.new(5, 'X', 5, 'X', :left),
+                              TMRule.new(5, '_', 1, '_', :right)
                            ])
 p tape = Tape.new([], 'a', ['a', 'a', 'b', 'b', 'b', 'c', 'c', 'c'], '_')
 dtm = DTM.new(TMConfiguration.new(1, tape), [6], rulebook)
