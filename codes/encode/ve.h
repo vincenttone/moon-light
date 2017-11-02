40bc4c15e3b9a65ff1fcb15e24b03b7e9662e8a0
vincent.tone <tongwensong@gmail.com>
2017-11-02 12:27:52 +0000

    adjust ve

---
+++ codes/encode/ve.h
@@ -0,0 +1,12 @@
+#ifndef _VE_H_
+#define _VE_H_ 1
+
+typedef struct _VeData{
+	size_t is;
+	size_t os;
+	size_t len;
+	char *in;
+	char *out;
+} VeData;
+
+#endif
--- codes/encode/xencode.c
+++ codes/encode/xencode.c
@@ -2,6 +2,109 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include "ve.h"
+
+#define XE_MASK 0xff
+
+int ve_encode(VeData *vd)
+{
+	char a1;
+	char a2 = 0;
+	char *a3;
+	size_t i;
+	size_t j = 0;
+	size_t l; // left
+	size_t n = 6; // need
+	vd->len = 0;
+	for (i = 0; i< vd->is; i++) {
+		a1 = *(vd->in + i);
+		l = 8;
+		printf("in: %d\n", a1);
+	compute:
+		printf("left: %zu, need: %zu\n", l, n);
+		if (l >= n) {
+			l = l - n;
+			printf("((%d & %d) >> %zu) | %d\n", a1, (0xff << l) & 0xff, l, a2);
+			a2 = ((a1 & ((0xff << l) & 0xff)) >> l) | a2;
+			a3 = vd->out + j;
+			*a3 = a2;
+			printf("out: %d.\n", a2);
+			n = 6;
+			a2 = 0x0;
+			if (++j > vd->os) {
+				return -1;
+			}
+			goto compute;
+		} else if (l == 0) {
+			continue;
+		} else {
+			a2 = (((a1 << (8 - l)) & 0xff) >> (8 - n)) | a2;
+			n = n - l;
+			l = 0;
+		}
+	}
+	if (n > 0) {
+		a3 = vd->out + j;
+		*a3 = a2;
+		printf("out: %d\n", a2);
+	}
+	return n;
+}
+
+int ve_encode_v0(VeData *vd)
+{
+	uint32_t a1;
+	uint32_t *a2;
+	size_t i = 0;
+	size_t j;
+	vd->len = 0;
+	for (; i< vd->is; i++) {
+		j = 8;
+		printf("input: %d\n", *(vd->in + i));
+		switch (i % 3) {
+		case 0:
+			if (i != 0) {
+				printf("%d\n", a1);
+				a2 = (uint32_t*)(vd->out + (32 * i / 3));
+				*a2 = (*a2) | a1;
+				vd->len++;
+			}
+			a1 = 0x0;
+			a1 = ((*(vd->in + i) & 0xfc) << 22) | a1;
+			j -= 6;
+			if (j == 0) continue;
+			printf("A1: %d\n", a1);
+			a1 = ((*(vd->in + i) & 0x3) << 20) | a1;
+			j -= 2;
+			if (j == 0) continue;
+			printf("A1: %d\n", a1);
+		case 1:
+			a1 = ((*(vd->in + i) & 0xfa) << 12) | a1;
+			j -= 4;
+			if (j == 0) continue;
+			printf("A1: %d\n", a1);
+			a1 = ((*(vd->in + i) & 0xe) << 10) | a1;
+			printf("A1: %d\n", a1);
+			j -= 4;
+			if (j == 0) continue;
+		case 2:
+			a1 = ((*(vd->in + i) & 0x3) << 2) | a1;
+			printf("A1: %d\n", a1);
+			j -= 2;
+			if (j == 0) continue;
+			a1 = (*(vd->in + i) & 0x40) | a1;
+			printf("A1: %d\n", a1);
+			j -= 6;
+			if (j == 0) continue;
+		}
+	}
+	if (i % 3 != 0) {
+		printf("%d.\n", a1);
+		*a2 = (*a2) | a1;
+		vd->len++;
+	}
+	return i;
+}
 
 int xencode(char *in, size_t s1, char *out, size_t s2)
 {
@@ -12,118 +115,61 @@ int xencode(char *in, size_t s1, char *out, size_t s2)
 	size_t l = 0; // len
 	size_t k = 0;
 	memset(out, 0x0, s2);
+	o = (uint8_t*)out;
 	for (; i < s1; i++) {
 		a1 = (uint8_t)(*(in + i));
-		o = (uint8_t*)(out + l);
 		if (k == 0) {
-			a2 = a1 << 6 >> 2;
+			a2 = (a1 << 6 & XE_MASK) >> 2;
+			printf("A2.: %d, A1: %d, k: %zu\n", a2, a1, k);
 			k = 4;
-			*o = (*o) | a1;
+			*o = (*o) | (a1 >> 2);
+			printf("---output: %d.\n", *o);
+			o = (uint8_t*)(out + l);		
 			l += 8;
 		} else if (k == 1) {
 			*o = (*o) | a2 | (a1 >> 7);
-			l += 8;
+			printf("---output: %d..\n", *o);
 			o = (uint8_t*)(out + l);
-			a2 = a1 << 7 >> 2;
+			l += 8;
+			a2 = (a1 << 7 & XE_MASK) >> 2;
+			printf("A2: %d..\n", a2);
 			*o = (*o) | (a1 >> 2);
+			printf("---output: %d...\n", *o);
+			o = (uint8_t*)(out + l);
 			l += 8;
 			k = 7;
 		} else {
-			*o = (*o) | a2 | (a1 >> k);
+			*o = (*o) | a2 | (a1 >> (8 - k));
+			printf("---output: %d....\n", *o);
 			l += 8;
 			o = (uint8_t*)(out + l);
-			a2 = a1 << (8 - k) >> 2;
-			k = 6 - k;
+			a2 = (a1 << (8 - k) & XE_MASK) >> 2;
+			printf("A2: %d, A1: %d, k: %zu\n", a2, a1, k);
+			k = k - 2;
+			if (k == 0) {
+				*o = (*o) | a2;
+				printf("---output: %d......\n", *o);
+				l += 8;
+				o = (uint8_t*)(out + l);
+			}
 		}
 	}
 	if (k != 0) {
 		*o = (*o) | a2;
+		printf("---output: %d.....\n", *o);
 		l += 8;
 	}
 	return l;
 }
 
-int encode(char *in, size_t s, char *out)
-{
-	uint8_t mask;
-	uint8_t cur;
-	uint8_t *o;
-	size_t l = 0; // len
-	size_t c = 0;
-	size_t m;
-	size_t n = 0;
-	o = (uint8_t*)(out + l);
-	for (;c < s; c++) {
-		m = 8;
-		cur = (uint8_t)(*(in + c));
-		printf("read: %d\n", cur);
-	append:
-		if (m == 0) {
-			continue;
-		}
-		if (n == 0) {
-			goto cover;
-		}
-		if (m == 8 - n) {
-			mask = ~ (0x7f << m);
-			cur = cur & mask;
-			*o = (*o) | cur;
-			printf("output: %d.\n", *o);
-			o = o + 8;
-			l += m;
-			n = 0;
-		} else if (m > 8 - n) {
-			mask = ~ (0x7f << (8 - n));
-			cur = cur & mask;
-			*o = (*o) | cur;
-			printf("output: %d..\n", *o);
-			o = o + 8;
-			m = m + n - 8;
-			l += 8;
-			n = 0;
-			goto cover;
-		} else if (m < 8 - n) {
-			cur = (cur << (8 - m)) >> n;
-			*o = (*o) | cur;
-			n -= m;
-			m = 0;
-		}
-		continue;
-	cover:
-		printf("len: %zu, m: %zu, n: %zu\n", l, m, n);
-		if (m > 6) {
-			mask = (~ (0x7f << (m - 6))) & cur;
-			cur = cur >> 2;
-			*o = (*o) | cur;
-			printf("output: %d...\n", *o);
-			o = o + 8;
-			cur = mask;
-			m -= 6;
-			l += 8;
-			n = 0;
-			goto append;
-		} else {
-			cur = (cur << m) >> 2;
-			*o = (*o) | cur;
-			printf("keep: %d....\n", *o);
-			n = 6 - m;
-			m = 0;
-		}
-	}
-	printf("len: %zu, m: %zu, n: %zu\n", l, m, n);
-	return n;
-}
-
 int main()
 {
-	char *buf = calloc(8, sizeof(char));
-	size_t s = xencode("abcd", 4, buf, 8);
-	printf("rest count: %zu\n", s);
-	uint8_t *m;
-	size_t x = 0;
-	for (; x < 8; x++) {
-		m = (uint8_t*)(buf + x);
-		printf("%d\n", *m);
-	}
+	VeData *d = calloc(1, sizeof(VeData));
+	char a[4] = "abcd";
+	d->in = a;
+	d->is = 4;
+	d->out = malloc(sizeof(char) * 10);
+	d->os = 10;
+	size_t s = ve_encode(d);
 	return 0;
 }
